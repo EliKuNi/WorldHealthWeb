8d03d9bb3b21a7d978c9cfe3fe7f46f88e738bfe
Elizabeth <65304506+EliKuNi@users.noreply.github.com>
2021-12-11 22:39:42 +0100

    Add files via upload

---
+++ works.js
@@ -0,0 +1,47 @@
+/* By Elizabeth Kuleshova 
+     Creation date: 2/12/2021 */
+
+//toggle
+function fun() {
+  var dark = document.body;
+      dark.classList.toggle('LightMod');
+}
+
+
+var example = ['HTA', 'Health Tourism Assessment'];
+
+textSequence(0);
+  function textSequence(i) {
+
+  if (example.length > i) { setTimeout(function() {
+  document.getElementById("sequence").innerHTML = example[i];
+  textSequence(++i); }, 1200);
+
+// 1 second (in milliseconds)
+}
+
+else if (example.length == i) { // Loop
+
+textSequence(1);
+
+}}
+
+//search your condition
+function myFunction() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
